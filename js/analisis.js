cc93aa5760f23f1824823ad72881ea096650851c
Javier Pepe <javier.pepe@gmail.com>
2023-01-30 11:51:30 -0300

    ultimo commit del archivo analisis con el agregado del calculo de la media del top10

--- js/analisis.js
+++ js/analisis.js
@@ -9,7 +9,8 @@ function calcularMedianaSalarioPersona(nombrePersona) {
     const salarios = trabajos.map((trabajos) => trabajos.salario);
 
     const medianaSalarial = PlatziMath.calcularMediana(salarios);
-    console.log(nombrePersona + ': ' + medianaSalarial);
+    //console.log(nombrePersona + ': ' + medianaSalarial);
+    return medianaSalarial;
 }
 
 function calcularMedianaTodos() {
@@ -53,4 +54,71 @@ salarios.forEach(function(datos) {
     });
 });
 
+// Calcular la mediana de salarios de una empresa para un anio especifico
+function calcularMedianaSalarioEmpresaAnio(empresa,anio) {
+    if (!empresas[empresa]){
+        console.warn('La empresa no existe');
+        return false;
+    } else if (!empresas[empresa][anio]){
+        console.warn('El anio indicado para la empresa no tiene salarios registrados');
+        return false;
+    } else {
+        console.log(empresas[empresa][anio]);
+        return PlatziMath.calcularMediana(empresas[empresa][anio]);
+    }    
+}
 
+// calcular la mediana de salarios de una empresa
+function proyeccionMedianaSalarioEmpresa(empresa) {
+    if (!empresas[empresa]) {
+        console.warn('La empresa no existe');
+        return false;
+    } else {
+        console.group('proyeccion empresa');
+        const medianasYear = [];
+
+        Object.keys(empresas[empresa]).forEach(function(anio){
+            const medianaYear = PlatziMath.calcularMediana(empresas[empresa][anio]);
+            medianasYear.push(medianaYear);
+        });
+        console.log('medianas por year:');
+        console.log(medianasYear);
+
+        const porcentualesYear = [];
+        for (let i = 1; i < medianasYear.length; i++) {
+            porcentualesYear.push((medianasYear[i] - medianasYear[i-1]) / medianasYear[i-1]);
+        }
+        console.log('porcentuales por year:');
+        console.log(porcentualesYear);
+
+        const medianaPorcentualTotal = PlatziMath.calcularMediana(porcentualesYear);
+        const medianaSalarioProyectado = medianasYear[medianasYear.length - 1] * (1 + medianaPorcentualTotal);
+        
+        console.log('mediana porcentual y mediana salarial proyectada');
+        console.log(medianaPorcentualTotal);
+        console.log(medianaSalarioProyectado);
+
+        console.groupEnd('proyeccion empresa');
+        return medianaSalarioProyectado;
+    }
+}
+
+// Analisis general
+function medianaGeneral() {
+    const medianaUsuarios = salarios.map(persona => calcularMedianaSalarioPersona(persona.name));
+    const medianaPersonas = PlatziMath.calcularMediana(medianaUsuarios);
+    console.log(medianaUsuarios);
+    
+    return medianaPersonas;
+}
+
+function medianaTop10() {
+    const medianaUsuarios = salarios.map(persona => calcularMedianaSalarioPersona(persona.name));
+    PlatziMath.ordenarLista(medianaUsuarios);
+
+    const cantidadTop10 = Math.trunc((medianaUsuarios.length * 10) / 100);
+    const limiteInicial = medianaUsuarios.length - cantidadTop10;
+    const top10 = medianaUsuarios.slice(limiteInicial, medianaUsuarios.length);
+    
+    console.log(top10);
+}
